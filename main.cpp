bc5583365e682088b6cf8394a5ec5d0710d83977
Santi <sant8613@gmail.com>
2019-02-28 19:04:46 +0100

    Practica 2 1.0

---
+++ main.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <time.h>
+
+using namespace std;
+
+void Insercion(int *, int);
+
+int main(){
+    int n = 0;
+    int *v;
+    int semilla, ini, fin, centinela;
+
+    cout << "Semilla para generar aleatorios: ";
+    cin >> semilla;
+    cout << "Introduce tamanyo del vector: ";
+    cin >> n;
+    cout << "Posiciones INICIAL y FINAL del vector para mostrar\n";
+    cout << "INICIAL: ";
+    cin >> ini;
+    cout << "FINAL: ";
+    cin >> fin;
+    cout << "Mostrar tiempo de ejecucion (1:Si, 0:No): ";
+    cin >> centinela;
+
+    srand(semilla);
+    v = new int [n+1];
+
+    for (int i = 1; i <= n; i++){
+        v[i] = rand()%n;
+    }
+
+    cout << "ALGORITMO DE INSERCION\n";
+    cout << "CASO CUALQUIERA\n";
+    cout << "---------------\n";
+    cout << "Vector a ordenar: ";
+    for (int i = ini;i <= fin; i++){
+        cout << v[i] << " ";
+    }
+    Insercion(v,n);
+    cout << "\nVector ordenado: ";
+    for (int i = ini;i <= fin; i++){
+        cout << v[i] << " ";
+    }
+
+    return 0;
+}
+
+void Insercion(int *v, int n){
+    int i,j,x;
+
+    for(i = 2;i <= n; i++){
+        x = v[i];
+        j = i - 1;
+        while(j > 0 && v[j] > x){
+            v[j + 1] = v[j];
+            j = j - 1;
+        }
+        v[j+1] = x;
+    }
+}
